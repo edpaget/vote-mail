78c839c2056a533a8908f007c1cc87f046bf1d2e
Edward Paget <ed.paget@gmail.com>
2011-09-05 20:19:44 -0500

    Parser fully working

--- mail_receiver.rb
+++ mail_receiver.rb
@@ -2,20 +2,30 @@ require 'mail'
 require './processor'
 require './config/grammar'
 
-module MailParse
-  class MailCommand
-    def initialize(email)
-      @poster = email.sender.first
-      MailParse.read_body(email.body.decoded).each do |field|
-      end
-    end
+class MailCommand
+  attr_accessor :command, :args, :command_grammar
+
+  def initialize(email)
+    body_tokens = read_body(email.body.decoded)
+    @command_grammar = Grammar.const_get(body_tokens.slice!(0))
+    @command = @command_grammar[:command]
+    @args = parse_args(body_tokens)
   end
-  def build(email)
+
+  def valid_arg?(arg)
+    @command_grammar[:args].include? arg
   end
 
   def read_body(email_body)
-    split_body = email_body.split(/\s|"([^"]*)"/)
+    split_body = email_body.split(/\s|"([^"]*)"/).delete_if { |token| token == "" }
   end
-  
+
+  def exec_command(command)
+    command[:command].call(command[:args])
+  end
+
+  def parse_args(tokens)
+    tokens.map! { |item| if valid_arg? item then item.downcase.intern else item end}
+    Hash[*tokens]
+  end
 end
-  
--- test/mail_receiver_test.rb
+++ test/mail_receiver_test.rb
@@ -33,15 +33,29 @@ class MailReceiverTest < Test::Unit::TestCase
     end
   end
 
-  def test_receive
-    [@post_site, @post_self, @upvote, @downvote].each do |m|
-    end
-  end
-  
   def test_assembled_hash
-    test_hash = MailParse.build(@post_site)
-    assert_equals test_hash, {:post => "This is a pretty cool site",
-                              :poster => "sender@example.com",
-                              :url => "http://www.google.com"}
+    test_parse = MailCommand.new(@post_site)
+    compare_hash = { :title => "This is a pretty cool site", :url => "http://www.google.com" }
+    assert_equal compare_hash, test_parse.args
   end
+
+  def test_read_body
+    test_body = @post_site.body.decoded
+    test_object = MailCommand.new(@post_site)
+    assert_equal ['POST', 'TITLE', 'This is a pretty cool site', 'URL', 'http://www.google.com'], test_object.read_body(test_body) 
+  end
+
+  def test_valid_arg?
+    test_object = MailCommand.new(@post_site)
+    assert test_object.valid_arg?('URL')
+    
+  end
+
+  def test_parse_args
+    test_object = MailCommand.new(@post_site)
+    test_arg_hash = test_object.parse_args(['TITLE', 'Test', 'URL', 'www.a.com'])
+    assert_equal 'Test', test_arg_hash[:title]
+  end
+    
+
 end
